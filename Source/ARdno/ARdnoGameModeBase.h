5a2110cb4a8d28f64d72a2e8110f7368fedc87a1
Ondrej Fojtik <fojtik.ondrej@seznam.cz>
2020-10-10 19:20:06 +0200

    Initial commit

---
+++ Source/ARdno/ARdnoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ARdnoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARDNO_API AARdnoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
